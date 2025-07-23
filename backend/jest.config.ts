04c5a42ceab5d9796a1b45fe755402b7c5a11fad
Gabriel Ramos <luisgabrielramosrobles@gmail.com>
2025-07-23 16:24:37 +0000

    Add backend/jest.config.ts 1753287876710

---
+++ backend/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setup.ts'],
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testMatch: ['**/tests/**/*.test.ts'],
+  slowTestThreshold: 50,
+  testTimeout: 30000,
+}
+
+export default config
